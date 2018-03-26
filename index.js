1edbbf6b1a157ec353c0e5629979c3f2850485ee
Candice Bianca Brown <candicebrown97@gmail.com>
2018-03-26 03:57:04 +0000

    completed Fahrenheit & Celsius toggle - clear sky pic displays correctly, will add the other images; and description

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(position){
         latLon(position);
         console.log("lat: " + position.coords.latitude);
-        console.log(position.coords.longitude);
+        console.log("lon: " + position.coords.longitude);
       });
     }   
     function latLon(position){
@@ -27,10 +27,46 @@ $(document).ready(function(){
                 },
                 success: function(data){
                     console.log(data);
-                    console.log(data.name);
-                    console.log(data.main.temp);
+                    // console.log(data.name);
+                    // console.log(data.main.temp);
+                    var showMe = show(data);
                 }
             });
+    }
+    
+    function show(data){
+        var date = new Date();
+        var city = data.name;
+        // var temp = data.main.temp;
+        var fahTemp = data.main.temp;
+        fahTemp=fahTemp.toFixed(1);
+        var celTemp = (fahTemp - 32)*5/9;
+        celTemp = celTemp.toFixed(0);
+        
+        //Putting data in my HTML
+        $('#date').html("<p>" + date + "</p>");
+        $('#city').html("<h2>" + city + "</h2>");
+        
+        //Temps
+        $('.change1').html("<h2>" + fahTemp + "&#8457" + "<h2>");
+        $('.change2').html("<h2>" + celTemp + "&#8451" + "<h2>");
+        
+        //Toggling -- http://api.jquery.com/toggle/
+    	$("#switchIt").click(function() {
+    		$(".change1").toggle();
+    		$("#switchIt") + $(".change2").toggle();
+    	});
+        
+        
+        
+        //Icon Images
+        //$('myObject').css('background-image', 'url(' + imageUrl + ')');
+        
+        //clear sky
+        if(data.weather[0].icon === "01d" || "01n"){
+            $('body').css('background-image', 'url("https://images.pexels.com/photos/108941/pexels-photo-108941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")');
+        }
+        
     }
 });
 
